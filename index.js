d9c8481643e564980238e81e88ed66f3277d23d5
XiaoLFeng <gm@xiaolfeng.cn>
2021-05-02 00:36:09 +0800

    202-03-21编辑

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Application = require('@waline/vercel');
 
-module.exports = Application({
+module.exports = Waline({
   async postSave(comment) {
     // do what ever you want after save comment
   },
+  avatarCDN: ['https://gravatar.loli.net/avatar/'],
 });
+
+
