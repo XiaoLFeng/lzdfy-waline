6e14295d771a953bb88eb0f30bdc1e0206987fbd
XiaoLFeng <gm@xiaolfeng.cn>
2021-05-05 11:47:36 +0800

    更新屏蔽词汇

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Waline = require('@waline/vercel');
 
 module.exports = Waline({
-  forbiddenWords: ['习近平', '毛泽东','礼品购买','代发','价格实惠','物流'],
+  forbiddenWords: ['习近平', '毛泽东','礼品购买','代发','价格实惠','物流','快递','专业网站'],
 });
